ea5403e95701210b643cce906e7a8095549df7a7
Aditya Sharma <adityasharma1709@gmail.com>
2023-01-19 16:55:14 -0800

    as - lab01 react app

---
+++ lab01/hello-world/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lab 1 - Hello from Team 9 Music Recommendation!</h1>
+      
+      <div align="left">
+        <h3>Here is our team: </h3>
+        <ul>
+          <li>Aditya Sharma</li>
+          {/* TODO: Add your name's into this React App!*/}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
