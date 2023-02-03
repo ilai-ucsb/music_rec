c2a49a71f934b631cdf4b9f8dc370cb01885de63
Anmol Kapoor <akapoor202@ucsb.edu>
2023-02-02 16:15:09 -0800

    ak - a wrapper around spotify's recommendation model

--- backend/model.py
+++ backend/model.py
@@ -7,16 +7,32 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
-def base_model(*args, artist_id="0MXwnhyYMeEfij4dl2YIQ4", **kwargs):
+def base_model(artist_ids=None, genre_ids=None, track_ids=None, limit=10, country='US', **kwargs):
     """
-    This model gives you the same recommendatio
+    Get a list of recommended tracks for one to five seeds. (at least one of artist_ids,
+    genre_ids, track_ids are needed)
+
+    Args:
+        artist_ids (List[str]) - a list of artist IDs, URIs, or URLs
+        genre_ids (List[str]) - a list of genre IDs, URIs, or URLs
+        track_ids (List[str]) - a list of track IDs, URIs, or URLs
+        limit (int) - The maximum number of recommendations. Default: 10
+        country (str) - An ISO 3166-1 alpha-2 country code. If provided, all results
+            will be playable in this country. Default: "US"
+        **kwargs (dict) - Any additional keyword arguments requested by the spotify API
+
+    Returns:
+        List - A List of Recommended tracks given the input information above
     """
     spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
     results = spotify.recommendations(
-        seed_artists=[artist_id],
-        limit=10,
-        country='US'
+        seed_artists=artist_ids,
+        seed_genres=genre_ids,
+        seed_tracks=track_ids,
+        limit=limit,
+        country=country,
+        **kwargs
     )
 
     return results
