2554b767fc4e97b00d6a0355a83b3ef3ffc7b28e
Ian Lai <ilai@ucsb.edu>
2023-02-04 20:08:26 -0800

    IL - updated api

--- backend/flask-api-serverless/app.py
+++ backend/flask-api-serverless/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from SpotifyAPICaller import top_tracks
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/result', methods=["POST"], strict_slashes=False)
 def getSongs():
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -10,13 +10,14 @@ function SearchBar({ setSearchResult }) {
     e.preventDefault();
     let songParameters = {
       method: 'POST',
+      mode: 'cors',
       headers: {
         "Content-Type": 'application/json'
       },
       body: JSON.stringify(searchInput)
     };
 
-    await fetch('/result', songParameters)
+    await fetch('https://i2w798wse2.execute-api.us-east-1.amazonaws.com/result', songParameters)
       .then(response => response.json())
       .then(data => setSearchResult(data))
     setSearchInput("");
