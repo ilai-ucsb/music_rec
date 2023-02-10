5406e3a5453cf021b695aaa0fc76ebfa615b9d02
Ian Lai <ilai@ucsb.edu>
2023-02-09 16:22:53 -0800

    IL - fixed testing for backend

--- backend/flask-api-serverless/tests/test_SpotifyAPICaller.py
+++ backend/flask-api-serverless/tests/test_SpotifyAPICaller.py
@@ -5,4 +5,5 @@ from app import app
 
 def test_get_recommendations():
     response = app.test_client().post("/result", json="gangnam style")
-    assert(response.get_json != None and len(response.get_json()['name']) == 5)
+    print(len(response.get_json()['name'][0]))
+    assert(response.get_json != None and len(response.get_json()['name'][0]) == 5)
