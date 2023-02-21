14c8e920bc06863bb83f57d2e1a6284667c5279f
phafezi <81775860+phafezi@users.noreply.github.com>
2023-02-21 14:44:07 -0800

    Update loud.py

--- backend/loud.py
+++ backend/loud.py
@@ -7,6 +7,20 @@ Created on Tue Feb 21 13:21:26 2023
 
 import pandas as pd
 def getLoud(data, loud, num):
+    """
+    usage pass in the data in from of list of dictionaries
+    pass in desired number of song and loud 
+    get back song with closest absolute loudness
+    
+    
+    internals:
+    read the data and convert it to a panda Data Frame
+    add a distances column 
+    sort data frame by distances
+    drop the distances columns 
+    return the top num songs in form of list of dict same structure as the mock_db
+    """
+    
     df = pd.DataFrame(data)
     df['distances'] = abs(df["loudness"] - loud)
     df.sort_values("distances", ascending=True,  inplace=True)
