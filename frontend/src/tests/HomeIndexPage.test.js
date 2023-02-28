1e2300c1aa99be6ca972cbefa61ab0dd7aad8290
Mauricio Munoz <mau13-13@hotmail.com>
2023-02-28 15:46:50 -0800

    mmv - Put back erased tests

--- frontend/src/tests/HomeIndexPage.test.js
+++ frontend/src/tests/HomeIndexPage.test.js
@@ -10,4 +10,27 @@ describe("HomeIndexPage tests", () => {
         const element = screen.getByRole("searchbox")
         expect(element).toBeInTheDocument();
     })
+    it("should display the filter popup when clicked", () => {
+        render(<HomeIndexPage/>)
+        const filterBtn = screen.getByRole("button", {name: "filters"});
+        expect(filterBtn).toBeInTheDocument();
+        fireEvent.click(filterBtn);
+        const closeBtn = screen.getByRole("button", {name: "close"});
+        const filterText = screen.getByText("explicit:")
+        expect(closeBtn).toBeInTheDocument();
+        expect(filterText).toBeInTheDocument();
+    })
+    it("should change values when a different option is selected", () => {
+        render(<HomeIndexPage/>)
+        const filterBtn = screen.getByRole("button", {name: "filters"});
+        fireEvent.click(filterBtn);
+        userEvent.selectOptions(screen.getByTestId('select'), "1")
+        expect(screen.getByTestId("select").value).toBe("1")
+    })
+    it("should not display the filter popup when the close button is clicked", () => {
+        render(<HomeIndexPage/>)
+        fireEvent.click(screen.getByRole("button", {name: "filters"}));
+        fireEvent.click(screen.getByRole("button", {name: "close"}));
+        expect(screen.queryByText("explicit:")).not.toBeInTheDocument();
+    })
 })
