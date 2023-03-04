3da91e8209aee422890929679bfb35694821225f
Aditya Sharma <adityasharma1709@gmail.com>
2023-03-04 03:40:50 -0800

    as - format in 1 line

--- backend/database/song.py
+++ backend/database/song.py
@@ -3,16 +3,17 @@ class Song(object):
     GANGNAM_STYLE = "Gangnam Style"
     DESPACITO = "Despacito"
     INVALID_SONG = "nbdjs183hdjhkzxiuoq2uqejhsjhks"
-    
-    # SPOTIFY_API_GOLDEN_COLUMNS: set of strings used for testing
+
+    # SPOTIFY_API_GOLDEN_COLUMNS: set columns used for testing
     SPOTIFY_API_GOLDEN_COLUMNS = ('id', 'name', 'year',
                                   'explicit', 'duration_ms',
                                   'popularity', 'danceability',
                                   'energy', 'key','loudness',
                                   'mode', 'speechiness',
-                                  'acousticness','instrumentalness',
-                                  'liveness', 'valence',
-                                  'tempo', 'time_signature')
+                                  'acousticness',
+                                  'instrumentalness', 'liveness',
+                                  'valence', 'tempo',
+                                  'time_signature')
     """
     The Song object contains all features for each song.
 
