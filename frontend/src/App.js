fdbbcd5d1d5ee5b6cdb1b83612410580cc83f1e7
Mauricio Munoz <mau13-13@hotmail.com>
2023-03-07 17:36:03 -0800

    mmv - Remove login page imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import AboutIndexPage from './components/pages/AboutIndexPage';
 import HomeIndexPage from './components/pages/HomeIndexPage';
-import SpotifyLoginPage from './components/pages/SpotifyLoginPage';
 
 // App.js is only used to route pages
 
@@ -15,7 +14,6 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomeIndexPage/>} />
           <Route exact path='/about' element={<AboutIndexPage/>} />
-          <Route exact path='/login' element={<SpotifyLoginPage/>} />
         </Routes>
       </BrowserRouter>
     </div>
