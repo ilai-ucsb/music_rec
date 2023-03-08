1fce1c6fabb66c9a574352dbc366c9654bd2daa4
Ian Lai <ilai@ucsb.edu>
2023-03-07 23:16:43 -0800

    IL - added filter by year and added material ui library

---
+++ frontend/src/components/Slider.js
@@ -0,0 +1,50 @@
+import React, { useState }from 'react'
+import { Slider, Box } from '@mui/material';
+import "./pages/utils/Page.css"
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+const minDistance = 5;
+
+function SliderComp() {
+  const [value, setValue] = useState([1950, 2022]);
+
+  const handleChange = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (newValue[1] - newValue[0] < minDistance) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], 2022 - minDistance);
+        setValue([clamped, clamped + minDistance]);
+      } else {
+        const clamped = Math.max(newValue[1], minDistance);
+        setValue([clamped - minDistance, clamped]);
+      }
+    } else {
+      setValue(newValue);
+    }
+  };
+
+  return (
+    <Box sx={{ width: 300 }}>
+      <input type="text" readOnly={true} style={{width: "30%", maxHeight: "30px"}} value={value[0]} onChange={(e) => e.target.value = value[0]}/> - &nbsp;
+      <input type="text" readOnly={true} style={{width: "30%", maxHeight: "30px"}} value={value[1]} onChange={(e) => e.target.value = value[1]}/>
+      <Slider
+        getAriaLabel={() => 'Minimum distance shift'}
+        value={value}
+        onChange={handleChange}
+        min={1950}
+        max={2022}
+        valueLabelDisplay="auto"
+        getAriaValueText={valuetext}
+        disableSwap
+      />
+    </Box>
+  );
+}
+
+export default SliderComp
