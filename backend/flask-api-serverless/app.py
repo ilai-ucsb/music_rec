06b1b8d47771ffcf975274183b7d1dbc42550a5b
Anmol Kapoor <akapoor202@ucsb.edu>
2023-03-09 23:36:24 -0800

    ak - adding the API endpoint without tests

--- backend/flask-api-serverless/app.py
+++ backend/flask-api-serverless/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
+
+os.environ["PRODUCTION"] = True
 
 from SpotifyAPICaller import get_recommendation
+from SimilaritySearch import similarSongs
 
 app = Flask(__name__)
 CORS(app)
@@ -15,12 +19,27 @@ def getSongs():
         return jsonify({
             "ERROR": "the name field is invalid"
         }), 403
-    except Exception as exc:
+    except Exception as exc:  # return error message with copy of input response
         return jsonify({
-            "ERROR": f"An unknown error occurred: {exc}"
+            "ERROR": f"An unknown error occurred: {exc}",
+            "INPUT": response
         }), 500
     return jsonify({
         "name": result
+    }), 200
+
+@app.route('/similar', methods=["POST"], strict_slashes=False)
+def getSimilar():
+    response = request.get_json()
+    try:
+        songs = similarSongs(**response)
+    except Exception as exc:  # return error message with copy of input response
+        return jsonify({
+            "ERROR": f"An unknown error occurred: {exc}",
+            "INPUT": response
+        }), 500
+    return jsonify({
+        "similar_songs": songs
     }), 200
 
 if __name__ == '__main__':
