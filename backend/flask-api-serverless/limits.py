637de7fed417322831b54de5aeddcb1935516a9e
Anmol Kapoor <akapoor202@ucsb.edu>
2023-03-14 16:39:20 -0700

    ak - adding speechiness -- forgot to add it before

--- backend/flask-api-serverless/limits.py
+++ backend/flask-api-serverless/limits.py
@@ -68,9 +68,14 @@ VALIDATION_TABLE = {
         "maximum": 100,
         "expected_type": int
     },
+    "speechiness": {
+        "minimum": 0,
+        "maximum": 1,
+        "expected_type": float
+    },
     "tempo": {
         "minimum": 0,
         "maximum": 1015,
-        "expected_type": int
+        "expected_type": float
     },
 }
