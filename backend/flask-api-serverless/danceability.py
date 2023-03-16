420a651b545854426628823b19ee47653cab04ef
phafezi <81775860+phafezi@users.noreply.github.com>
2023-03-16 12:46:29 -0700

    Update backend/flask-api-serverless/danceability.py
    Co-authored-by: Anmol Kapoor <41317321+Gopu2001@users.noreply.github.com>

--- backend/flask-api-serverless/danceability.py
+++ backend/flask-api-serverless/danceability.py
@@ -27,6 +27,6 @@ def getDanceability(data, danceability_level, num_results):
     df.sort_values("distances", ascending=True,  inplace=True)
     idx = df.columns.get_loc("danceability")
     df.drop(df.columns[idx], axis=1, inplace=True)
-    df = df.head(num)
+    df = df.head(num_results)
     
     return df.to_dict('records')
